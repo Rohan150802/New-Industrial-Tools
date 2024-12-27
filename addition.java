c4edcdcac7e52faa9b69cc30edecb8d7c5a72b8c
Rohan150802 <rohandpatil158@gmail.com>
2024-12-27 18:06:21 +0530

    this is modified file

--- addition.java
+++ addition.java
@@ -1,11 +1,12 @@
 public class addition{
+
+public static void main(String args[]){
+
 int a=10;
 int b=10;
 int c;
 
+System.out.println("the addition of c : "+c);
+
 }
-public static void main(String args[]){
-
-System.out.println("the addition of c : "+c)
-
 }
